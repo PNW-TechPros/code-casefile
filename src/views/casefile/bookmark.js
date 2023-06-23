cf0a18272ca470dd97b496aa29c48336bc993b9c
Richard T. Weeks <rtweeks21@gmail.com>
2023-06-23 07:17:25 -0700

    Implement a basic view of the bookmark forest

--- src/views/casefile/bookmark.js
+++ src/views/casefile/bookmark.js
@@ -1,14 +1,41 @@
+import React from 'preact/compat'; // This just makes VSCode's Intellisense happy
 import { $bookmark } from "../../datumPlans";
+import { omit } from 'lodash';
 
-const Bookmark = ({ tree }) => {
+const LineRef = ({ bookmark }) => (
+    <div className="line-ref">
+        {$bookmark.file.get(bookmark)}:{$bookmark.line.get(bookmark)}
+    </div>
+);
+
+const TargetText = ({ bookmark }) => (
+    <div className="tagged-content">
+        {$bookmark.markText.get(bookmark)}
+    </div>
+);
+
+const MarkInfo = ({ bookmark }) => {
+    const openThis = () => {};
+    return (
+        $bookmark.file.get(bookmark)
+        ? (
+            <div onClick={openThis}>
+                <LineRef bookmark={bookmark}/>
+                <TargetText bookmark={bookmark}/>
+            </div>
+        )
+        : <h3>{$bookmark.markText.get(bookmark)}</h3>
+    );
+};
+
+const Bookmark = ({ tree: treeNode }) => {
+    // Eventually, this should be a drag source
     return (
         <div>
-            <div>
-                {$bookmark.markText.get(tree)}
-            </div>
-            <div class="bookmark-children">
+            <div className="bookmark"><MarkInfo bookmark={omit(treeNode, 'children')}/></div>
+            <div className="bookmark-children">
                 {...Array.from(
-                    $bookmark.children.getIterable(tree),
+                    $bookmark.children.getIterable(treeNode),
                     subTree => <Bookmark tree={subTree} key={subTree.id}/>
                 )}
             </div>
