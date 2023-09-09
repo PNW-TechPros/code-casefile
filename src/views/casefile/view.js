2c78336facd72ba4ff1cce3d6c46bfd0df696c37
Richard T. Weeks <rtweeks21@gmail.com>
2023-09-09 12:15:33 -0700

    Display trash during drag
    Issues: #1

--- src/views/casefile/view.js
+++ src/views/casefile/view.js
@@ -1,9 +1,9 @@
 import { render } from 'preact';
 import React from 'preact/compat'; // This just makes VSCode's Intellisense happy
-import { useEffect, useState } from 'preact/hooks';
+import { useEffect, useRef, useState } from 'preact/hooks';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
-import getMessageHandlers from './receivedMessages';
+import { applicationOfMessagesToState } from './receivedMessages';
 import { thru } from 'lodash';
 import { $casefile } from '../../datumPlans';
 import Bookmark from './bookmark';
@@ -12,33 +12,53 @@ import { MessagePasser } from './messageSending';
 
 const vscode = acquireVsCodeApi();
 
+const useDragging = () => {
+    const [dragging, setDragging] = useState(false);
+    const dragWatcher = useRef();
+    useEffect(() => {
+        const node = dragWatcher.current;
+        const listeners = [
+            ['dragstart',   () => { setDragging(true); },   true],
+            ['dragend',     () => { setDragging(false); },  false],
+        ];
+        for (const listenArgs of listeners) {
+            node.addEventListener(...listenArgs);
+        }
+        // Remove listeners when unmounted
+        return () => {
+            for (const listenArgs of listeners) {
+                node.removeEventListener(...listenArgs);
+            }
+        };
+    }, []);
+
+    return [dragging, dragWatcher];
+};
+
 const View = () => {
     const [state, setState] = thru(
         useState(vscode.getState() || {}),
         ([state, setState]) => [state, (newState) => {setState(newState); vscode.setState(newState);}]
     );
+    useEffect(applicationOfMessagesToState(state, setState), []);
 
-    useEffect(() => thru(
-        getMessageHandlers({ context: { getState: () => state, setState } }),
-        handlers => {
-            const handleEvent = (event) => {
-                handlers.dispatch(event.data);
-            };
-            window.addEventListener('message', handleEvent);
-            return () => window.removeEventListener('message', handleEvent);
-        }
-    ), []);
+    const [dragging, dragWatcherRef] = useDragging();
 
     return (
         <MessagePasser value={(data) => {vscode.postMessage(data);}}>
             <DndProvider backend={HTML5Backend}>
-                <div className="casefile-ui">
+                <div className="casefile-ui" ref={dragWatcherRef}>
                     <div className="bookmarks-forest">
                         {...Array.from(
                             $casefile.bookmarks.getIterable(state),
                             bookmark => <Bookmark tree={bookmark} key={bookmark.id}/>
                         )}
                     </div>
+                    { dragging && (
+                        <div className="bookmark-trash">
+                            <div><i className="codicon codicon-trash"></i> Remove</div>
+                        </div>
+                    )}
                 </div>
             </DndProvider>
         </MessagePasser>
@@ -46,3 +66,4 @@ const View = () => {
 };
 
 render(<View />, document.body);
+
